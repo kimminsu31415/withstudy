0c88477753a2c9657f8c5541666d5b44835e18ae
minsu kim <kimminsu31415@gmail.com>
2025-01-28 23:08:15 +0900

    fix: 스터디룸 메시지 삭제

--- src/pages/StudyRoom.jsx
+++ src/pages/StudyRoom.jsx
@@ -5,11 +5,6 @@ import Tool from '../components/Tool';
 function StudyRoom() {
   return (
     <div style={styles.container}>
-      <h2>Study Room</h2>
-      <p>
-        이곳에서 화면 공유, 유튜브 링크 추가, 스터디 타이머 등을 구현할
-        예정입니다.
-      </p>
       <Tool />
     </div>
   );
