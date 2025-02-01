565f51702fda369a6c578b2af520efb50ce07b9e
minsu kim <kimminsu31415@gmail.com>
2025-02-01 21:45:15 +0900

    add: test page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import StudyRoom from './pages/StudyRoom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import StudyRoom from "./pages/StudyRoom";
+import Test from "./pages/Test";
 
 function App() {
   return (
@@ -10,10 +11,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/studyroom" element={<StudyRoom />} />
+        <Route path="/test" element={<Test />} />
         <Route
           path="*"
           element={
-            <div style={{ textAlign: 'center', padding: '2rem' }}>
+            <div style={{ textAlign: "center", padding: "2rem" }}>
               <h2>404 - 페이지를 찾을 수 없습니다.</h2>
               <a href="/">홈으로 돌아가기</a>
             </div>
