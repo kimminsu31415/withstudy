8551a2791ce5ff60f134e9f99cea3f4bd14099cb
minsu kim <kimminsu31415@gmail.com>
2025-02-04 20:50:05 +0900

    fix: 영상 화면 반응형 수정

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -4,14 +4,12 @@ function Test() {
   return (
     <div className="relative flex min-h-screen w-full bg-[#222222] text-white">
       {/* 메인콘텐츠 */}
-      <div className="mx-auto w-full max-w-[calc(100%-80px)] bg-slate-500">
-        <div className="relative w-full">
-          {/* 16:9 aspect ratio wrapper */}
-          <div className="pb-[56.25%]">
-            <div className="absolute left-0 top-0 h-full w-full bg-slate-300">
-              1번 동영상
-            </div>
-          </div>
+      <div className="mx-auto flex w-full max-w-[calc(100%-80px)] items-center justify-center bg-slate-500">
+        {/* 영상 */}
+        <div className="flex w-full justify-center gap-5 bg-red-300">
+          {/* flex-1로 각 요소가 같은 너비를 갖도록 하고, w-full을 추가하여 가로폭을 채우도록 설정 */}
+          <div className="aspect-video flex-1 bg-slate-300">1번 동영상</div>
+          <div className="aspect-video flex-1 bg-slate-300">2번 동영상</div>
         </div>
       </div>
 
