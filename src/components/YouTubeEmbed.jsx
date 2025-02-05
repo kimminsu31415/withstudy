ac7422473419304d7f27cb5d35366f6e02b52a6d
minsu kim <kimminsu31415@gmail.com>
2025-02-05 22:22:23 +0900

    fix: youtube embed size

--- src/components/YouTubeEmbed.jsx
+++ src/components/YouTubeEmbed.jsx
@@ -3,10 +3,9 @@ import React from "react";
 
 function YouTubeEmbed({ url }) {
   return (
-    <div className="aspect-video w-[640px] max-w-full bg-black">
-      {/* iframe 내비둬도 되고, style 원하는 대로 */}
+    <div className="w-[640px] max-w-full aspect-video bg-black">
       <iframe
-        className="h-full w-full"
+        className="w-full h-full"
         src={url}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
