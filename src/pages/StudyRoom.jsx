3e8d78310e3c201b000ea61234fa44084eda1ff1
minsu kim <kimminsu31415@gmail.com>
2025-02-13 22:16:06 +0900

    fix: studyroom

--- src/pages/StudyRoom.jsx
+++ src/pages/StudyRoom.jsx
@@ -4,12 +4,12 @@ import MainContent from "../components/MainContent";
 import Tool from "../components/Tool";
 
 function StudyRoom() {
-  // 영상 URL을 저장할 상태. 없으면 null
+  // 영상 URL을 저장할 상태
   const [videoUrl, setVideoUrl] = useState(null);
 
   return (
     <div className="flex h-screen flex-col bg-[#222222] text-center font-Pretendard">
-      {/* 메인 콘텐츠 (flex-1로 툴 제외한 남은 공간 채우기) */}
+      {/* 메인 콘텐츠 */}
       <div className="flex-1">
         <MainContent videoUrl={videoUrl} />
       </div>
